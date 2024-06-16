7fb4af2f696d227e1f4f3c1960a111fd15cc29de
albanybuipe96 <mohammedarahman96@gmail.com>
2024-06-16 23:27:28 +0000

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="wrapper">
     <h1>Hello I'm in App.vue</h1>
-    <HelloWorld msg="You did it!" />
   </div>
-
 </template>
-
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<style scoped>
-h1 {
-  color: purple;
-}
-</style>
