7c9fd8a9a4b406eb613b74727f78e2c1f97db3f6
albanybuipe96 <mohammedarahman96@gmail.com>
2024-06-17 17:38:24 +0000

    updated custom brand colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       },
       colors: {
         'brand-gray-1': '#dadce0',
-        'brand-blue-1': '#1967a2',
+        'brand-blue-1': '#1967d2',
         'brand-green-1': '#137333'
       },
       boxShadow: {
