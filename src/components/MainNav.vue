8c43be9c5685e891927ddda68f711c185ccf8dc0
albanybuipe96 <mohammedarahman96@gmail.com>
2024-06-17 14:48:31 +0000

    tested menu items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,18 +2,44 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8">
-        <a href="#" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="ml-9 h-full first:ml-0" v-for="option in menu" :key="option.name">
+              <a :href="option.url" class="flex h-full items-center py-2.5">{{ option.name }}</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="ml-auto flex h-full items-center py-2.5">
+          <action-button />
+          <profile-image />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from '@/components/widgets/ActionButton.vue'
+import ProfileImage from './widgets/ProfileImage.vue'
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton,
+    ProfileImage
+  },
   data() {
     return {
-      company: 'Nexus Careers'
+      company: 'Nexus Careers',
+      url: 'https://careers.google.com',
+      menu: [
+        { name: 'Teams', url: '' },
+        { name: 'Locations', url: '' },
+        { name: 'Life at Nexus Careers', url: '' },
+        { name: 'How we hire', url: '' },
+        { name: 'Students', url: '' },
+        { name: 'Jobs', url: 'https://careers.google.com' }
+      ]
     }
   }
 }
--- tests/components/MainNav.test.js
+++ tests/components/MainNav.test.js
@@ -8,4 +8,17 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Nexus Careers')
     expect(companyName).toBeInTheDocument()
   })
+  it('displays menu items for navigation', () => {
+    render(MainVue)
+    const navMenu = screen.getAllByRole('listitem')
+    const navItems = navMenu.map((item) => item.textContent)
+    expect(navItems).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Nexus Careers',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+  })
 })
