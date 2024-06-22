acc06d896b86040a06abaa781f25fe2608128ab5
albanybuipe96 <mohammedarahman96@gmail.com>
2024-06-22 13:02:47 +0000

    created job page

---
+++ src/pages/JobPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Job Page for {{ currentJobId }}</div>
+</template>
+
+<script>
+export default {
+  name: 'JobPage',
+  components: {},
+  computed: {
+    currentJobId() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,23 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from '@/pages/HomePage.vue'
 import JobResultsPage from '@/pages/JobResultsPage.vue'
+import JobPage from '@/pages/JobPage.vue'
 
 const routes = [
   { path: '/', component: HomePage, name: 'Home' },
   { path: '/jobs/results', component: JobResultsPage, name: 'JobResults' },
+  { path: '/jobs/results/:id', component: JobPage, name: 'JobListing' }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+export class AppRoutes {
+  static Home = 'Home'
+  static JobResults = 'JobResults'
+  static JobListing = 'JobListing'
+}
 
 export default router
